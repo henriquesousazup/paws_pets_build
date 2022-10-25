a571ff74f611b8d91c3ef1be49408ec9c6743faf
Henrique Sousa <you@example.com>
2022-10-25 13:32:50 -0300

    testando

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-docker run -d --name z-coders-henrique-sousa -p 8080:8080 z-coders-henrique-sousa:release
+docker run -d --name z-coders-henrique-sousa -p 8080:8080 z-coders-henrique-sousa
